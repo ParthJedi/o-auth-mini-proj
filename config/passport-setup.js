1e0cd335a5cefc2bca663d42c82e567abb2c789d
Parth Joshi <parth.joshi@asianetnews.in>
2020-03-18 18:15:56 +0530

    new user check.

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,6 +1,7 @@
 const passport =  require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 const keys = require('./keys');
+const User = require('../models/user-model');
 
 passport.use(
     new GoogleStrategy({
@@ -12,6 +13,21 @@ passport.use(
     // cb function for passport
     console.log('passport cb fired!');
     console.log(profile);
+    // check if user exists
+    User.findOne({googleId: profile.id}).then((currentUser) => {
+        if(currentUser) {
+            // already exists
+            console.log('user is: ' + currentUser);
+        } else {
+            new User({
+            username: profile.displayName,
+            googleId: profile.id
+            }).save().then((newUser) => {
+                console.log('new user created: '+ newUser);
+            })
+        }
+    })
+
     // done();
 })
 )
